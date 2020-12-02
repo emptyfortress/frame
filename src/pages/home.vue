f6851eef174c33afa3b0d3b294c064480ff183eb
Volkov Alexander <volkov.525@gmail.com>
2020-12-02 18:33:39 +0300

    just check

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -5,11 +5,11 @@
       <f7-nav-left>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
       </f7-nav-left>
-      <f7-nav-title sliding>frame</f7-nav-title>
+      <f7-nav-title sliding>frame rules</f7-nav-title>
       <f7-nav-right>
         <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="right"></f7-link>
       </f7-nav-right>
-      <f7-nav-title-large>frame</f7-nav-title-large>
+      <f7-nav-title-large>frame cool</f7-nav-title-large>
     </f7-navbar>
     <!-- Toolbar-->
     <f7-toolbar bottom>
